ff6742262003be3ac122f621d9efb1ba657f0f88
Lars <larsriel@gmail.com>
2021-12-02 15:55:18 +0100

    navstuff

--- navbar.js
+++ navbar.js
@@ -5,3 +5,19 @@ signup.setAttribute('href', '/signup');
 login.setAttribute('href', '/login');
 
 console.log('asdf');
+
+fetch('http://localhost:8080/api/public/login', {
+  method: 'POST',
+  mode: 'no-cors',
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+  body: JSON.stringify({
+    username: 'jazzymcjazz',
+    password: 'savemeperi',
+  }),
+})
+  .then((Response) => Response.json())
+  .then((response) => {
+    console.log(response);
+  });
