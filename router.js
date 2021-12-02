8e983e55d0fd0510ff2bc289d34967dfd8a20bb3
alexhersfeldt <39616651+alexhersfeldt@users.noreply.github.com>
2021-12-02 15:57:27 +0100

    hello

--- router.js
+++ router.js
@@ -1,4 +1,5 @@
-import renderMain from '/pages/main/main.js';
+import renderMain from './pages/main/main.js';
+import renderLogin from './pages/login/login.js';
 
 export default () => {
   const router = new Navigo('/', { hash: true });
@@ -8,6 +9,10 @@ export default () => {
       '/': () => {
         renderMain().then(router.updatePageLinks);
         console.log('User requested main page');
+      },
+      login: () => {
+        renderLogin();
+        console.log('User requested login page');
       },
     })
     .resolve();
