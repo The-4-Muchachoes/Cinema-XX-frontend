13c729f47d70bfb75bf1ebdf9f7d712e4a9e7c65
Lars <larsriel@gmail.com>
2021-12-08 16:30:19 +0100

    refactor

--- pages/screening/screening.js
+++ pages/screening/screening.js
@@ -8,12 +8,34 @@ export default () => {
       });
   };
 
-
-const apiUrl = 'http://api.jazzymcjazz.dk/api/public/screenings/2/14-10-2021/20-11-2022'
+const apiUrl = 'http://api.jazzymcjazz.dk/api/public/screenings/2/14-10-2021/20-11-2022';
 fetch(apiUrl)
-    .then(response => response.json())
-    .then(movieData => {
-        console.log(movieData);
+.then(response => response.json())
+.then(movieData => {
+        movieData.forEach(movie => {
+          const movieContainer = document.createElement('div');
+          movieContainer.classList.add('movieContainer');
+
+          const movieTitle = document.createElement('h1');
+          const movieImage = document.createElement('img');
+          
+          const movieTickets = document.createElement('button');
+          movieTickets.addEventListener('click', () => {
+            console.log(movie.movie.trailerUrl);
+          });
+
+          movieTickets.innerHTML = 'Monkey';
+
+          movieImage.src = movie.movie.posterUrl;
+          movieTitle.innerHTML = movie.movie.title;
+          
+
+          movieContainer.appendChild(movieImage);
+          movieContainer.appendChild(movieTitle);
+          movieContainer.appendChild(movieTickets);
+      
+          document.getElementById('screenings').appendChild(movieContainer);
+      });
     })   
 
     
