7c38370a004812d7379a7649ca1ddc10ee6b6215
Chris <christophermillington1@gmail.com>
2021-12-08 23:25:28 +0100

    screening runnning

--- pages/screening/screening.js
+++ pages/screening/screening.js
@@ -1,45 +1,46 @@
 export default () => {
-    const content = document.querySelector('.content');
-  
-    return fetch('./pages/screening/screening.html')
-      .then((Response) => Response.text())
-      .then((screeningHtml) => {
-        content.innerHTML = screeningHtml;
-      });
-  };
+  const content = document.querySelector('.content');
 
+  return fetch('./pages/screening/screening.html')
+    .then((Response) => Response.text())
+    .then((screeningHtml) => {
+      content.innerHTML = screeningHtml;
+    });
+};
+
+document.addEventListener("DOMContentLoaded", function() {
 const apiUrl = 'http://api.jazzymcjazz.dk/api/public/screenings/1/14-10-2021/20-11-2022';
 fetch(apiUrl)
 .then(response => response.json())
 .then(movieData => {
   //testing what movieData returns
   console.log(movieData);
-        movieData.forEach(movie => {
-          //testing what movie returns
-          console.log(movie);
-          const movieContainer = document.createElement('div');
-          movieContainer.classList.add('movieContainer');
+  movieData.forEach(movie => {
+    //testing what movie returns
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movieContainer');
 
-          const movieTitle = document.createElement('h1');
-          const movieImage = document.createElement('img');
-          
-          const movieTickets = document.createElement('button');
-          movieTickets.addEventListener('click', () => {
-            console.log(movie.movie.trailerUrl);
-          });
+    const movieTitle = document.createElement('h1');
+    const movieImage = document.createElement('img');
+    
+    const movieTickets = document.createElement('button');
+    movieTickets.addEventListener('click', () => {
+      console.log(movie.movie.trailerUrl);
+    });
 
-          movieTickets.innerHTML = 'Buy Ticket';
+    movieTickets.innerHTML = 'Buy Ticket';
 
-          movieImage.src = movie.movie.posterUrl;
-          movieTitle.innerHTML = movie.movie.title;
-          
+    movieImage.src = movie.movie.posterUrl;
+    movieTitle.innerHTML = movie.movie.title;
+    
 
-          movieContainer.appendChild(movieImage);
-          movieContainer.appendChild(movieTitle);
-          movieContainer.appendChild(movieTickets);
-      
-          document.querySelector('.screenings').appendChild(movieContainer);
-      });
-    })   
+    movieContainer.appendChild(movieImage);
+    movieContainer.appendChild(movieTitle);
+    movieContainer.appendChild(movieTickets);
+
+    document.querySelector('.screenings').appendChild(movieContainer);
+  });
+});    
+});  
 
     
