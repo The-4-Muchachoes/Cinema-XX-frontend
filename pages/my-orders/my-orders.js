3d8c76d398f64a7a738303a705f3757e9cc7204b
Lars <larsriel@gmail.com>
2021-12-08 20:18:40 +0100

    implemented my-orders page with cancel option

--- pages/my-orders/my-orders.js
+++ pages/my-orders/my-orders.js
@@ -1,6 +1,9 @@
 export default () => {
   const content = document.querySelector('.content');
 
+  const user = JSON.parse(localStorage.getItem('user'));
+  if (user == null || user.accessToken == null) window.location.href = '/';
+
   return fetch('./pages/my-orders/my-orders.html')
     .then((Response) => Response.text())
     .then((myOdersHtml) => {
@@ -39,15 +42,16 @@ function renderOrders(orders) {
     const time = datetime[1].slice(0, 5);
 
     const orderDiv = document.createElement('div');
-    orderDiv.innerHTML = `
-            <div class="orders-movie-title">Movie ${order.screening.movie.title}</div>
-            <div class="order-info">Theater: ${order.screening.theater.name}</div>
-            <div class="order-info">Date: ${date}</div>
-            <div class="order-info">Time: ${time}</div>
-            <div class="order-info">Seats: </div>
-            <div class="order-info">Status: ${order.status}</div>
-          `;
+    orderDiv.setAttribute('class', 'order flex');
+    orderDiv.innerHTML = getOrderHtml(order, date, time);
+
+    ordersDiv.appendChild(document.createElement('hr'));
     ordersDiv.appendChild(orderDiv);
+
+    if (order.status == 'CONFIRMED') {
+      const cancelTd = document.querySelector(`#cancel-td-${order.id}`);
+      cancelTd.appendChild(getCancelButton(order));
+    }
   });
 }
 
@@ -58,4 +62,80 @@ function formatDate(date) {
   const day = dateList[2];
 
   return `${day}-${month}-${year}`;
+}
+
+function getOrderHtml(order, date, time) {
+  return `
+  <table>
+    <tbody>
+      <tr>
+        <td rowspan="6">
+          <img src="${order.screening.movie.posterUrl}"/>
+        </td>
+        <td>Movie:</td>
+        <td>${order.screening.movie.title}</td>
+        <td class="cancel-td" id="cancel-td-${order.id}" rowspan="6"></td>
+      </tr>
+      <tr>
+        <td>Theater:</td>
+        <td>${order.screening.theater.name}</td>
+      </tr>
+      <tr>
+        <td>Date:</td>
+        <td>${date}</td>
+      </tr>
+      <tr>
+        <td>Time:</td>
+        <td>${time}</td>
+      </tr>
+      <tr>
+        <td>Status:</td>
+        <td>${order.status}</td>
+      </tr>
+      <tr>
+        <td>Seats:</td>
+        <td>${getSeatHtml(order.seats)}</td>
+      </tr>  
+    </tbody>
+  </table>
+  `;
+}
+
+function getSeatHtml(seats) {
+  let seatHtml = ``;
+
+  seats.forEach((seat) => {
+    seatHtml += `
+      <div>Row: ${seat.row}, Seat: ${seat.seatNo}</div>
+    `;
+  });
+
+  return seatHtml;
+}
+
+function getCancelButton(order) {
+  const btn = document.createElement('button');
+  btn.setAttribute('class', 'cancel-tickets');
+  btn.setAttribute('title', 'Cancel Tickets');
+  btn.innerHTML = 'X';
+  addCancelTicketsEventListener(btn, order);
+
+  return btn;
+}
+
+function addCancelTicketsEventListener(button, order) {
+  button.addEventListener('click', () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    fetch(`${window.apiUrl}/api/user/bookings/${order.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        Authorization: 'Bearer ' + user.accessToken,
+      },
+    }).then((Response) => {
+      console.log(Response);
+      window.location.reload();
+    });
+  });
 }
